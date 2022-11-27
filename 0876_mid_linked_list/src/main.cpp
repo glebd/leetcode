011b842d6dc75573c32c16c7184a11ef878f1fc8
Gleb Dolgich <glebd70@gmail.com>
2022-11-27 17:36:44 +0000

    Add failing tests (and memory leaks)

--- 0876_mid_linked_list/src/main.cpp
+++ 0876_mid_linked_list/src/main.cpp
@@ -1,16 +1,16 @@
 #include <gtest/gtest.h>
 
+#include <vector>
+
 // PR: https://github.com/glebd/leetcode/pull/new/0876_mid_linked_list
 
 // Definition for singly-linked list.
 struct ListNode
 {
-    int val;
-    ListNode* next;
-    ListNode() : val(0), next(nullptr)
-    {
-    }
-    ListNode(int x) : val(x), next(nullptr)
+    int val{0};
+    ListNode* next{nullptr};
+    ListNode() = default;
+    ListNode(int x) : val(x)
     {
     }
     ListNode(int x, ListNode* next) : val(x), next(next)
@@ -23,11 +23,49 @@ class Solution
 public:
     ListNode* middleNode(ListNode* head)
     {
+        if (head == nullptr)
+            return nullptr;
+
         return nullptr;
     }
 };
 
+/*
+std::vector<ListNode*> vector2list(const std::vector<int>& vec)
+{
+    return nullptr;
+}
+ */
+
+TEST(MidLinkedList, TestNull)
+{
+    Solution solution;
+    auto* actual = solution.middleNode(nullptr);
+    ASSERT_EQ(actual, nullptr);
+}
+
 TEST(MidLinkedList, Test1)
 {
-    ASSERT_TRUE(true);
+    //std::vector data = {1, 2, 3, 4, 5};
+    auto* elem5 = new ListNode(5);
+    auto* elem4 = new ListNode(4, elem5);
+    auto* elem3 = new ListNode(3, elem4);
+    auto* elem2 = new ListNode(2, elem3);
+    auto* head = new ListNode(1, elem2);
+    Solution solution;
+    auto* actual = solution.middleNode(head);
+    ASSERT_EQ(actual, elem3);
+}
+
+TEST(MidLinkedList, Test2)
+{
+    auto* elem6 = new ListNode(6);
+    auto* elem5 = new ListNode(5, elem6);
+    auto* elem4 = new ListNode(4, elem5);
+    auto* elem3 = new ListNode(3, elem4);
+    auto* elem2 = new ListNode(2, elem3);
+    auto* head = new ListNode(1, elem2);
+    Solution solution;
+    auto* actual = solution.middleNode(head);
+    ASSERT_EQ(actual, elem4);
 }

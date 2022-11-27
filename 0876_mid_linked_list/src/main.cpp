f931d2d7d0a86bb4504803b0031d0a3b7518d668
Gleb Dolgich <glebd70@gmail.com>
2022-11-27 19:47:18 +0000

    Mark ctor explicit

--- 0876_mid_linked_list/src/main.cpp
+++ 0876_mid_linked_list/src/main.cpp
@@ -10,7 +10,7 @@ struct ListNode
     int val{0};
     ListNode* next{nullptr};
     ListNode() = default;
-    ListNode(int x) : val(x)
+    explicit ListNode(int x) : val(x)
     {
     }
     ListNode(int x, ListNode* next) : val(x), next(next)

33d0debd4b0129209e0eedd5add4c862f7e5317a
Gleb Dolgich <glebd70@gmail.com>
2022-11-27 17:09:10 +0000

    Add skeleton code from problem

--- 0876_mid_linked_list/src/main.cpp
+++ 0876_mid_linked_list/src/main.cpp
@@ -1,5 +1,32 @@
 #include <gtest/gtest.h>
 
+// PR: https://github.com/glebd/leetcode/pull/new/0876_mid_linked_list
+
+// Definition for singly-linked list.
+struct ListNode
+{
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr)
+    {
+    }
+    ListNode(int x) : val(x), next(nullptr)
+    {
+    }
+    ListNode(int x, ListNode* next) : val(x), next(next)
+    {
+    }
+};
+
+class Solution
+{
+public:
+    ListNode* middleNode(ListNode* head)
+    {
+        return nullptr;
+    }
+};
+
 TEST(MidLinkedList, Test1)
 {
     ASSERT_TRUE(true);

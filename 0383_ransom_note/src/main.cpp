d3f94e5818b4b025545053bdb91c9470d3799bd0
Gleb Dolgich <glebd70@gmail.com>
2022-12-07 18:21:26 +0000

    Add failing tests

--- 0383_ransom_note/src/main.cpp
+++ 0383_ransom_note/src/main.cpp
@@ -1,6 +1,30 @@
 #include <gtest/gtest.h>
 
+#include <string>
+
+class Solution
+{
+public:
+    bool canConstruct(std::string ransomNote, std::string magazine)
+    {
+        return false;
+    }
+};
+
 TEST(RansomNote, Test1)
 {
-    ASSERT_TRUE(true);
+    Solution solution;
+    ASSERT_FALSE(solution.canConstruct("a", "b"));
+}
+
+TEST(RansomNote, Test2)
+{
+    Solution solution;
+    ASSERT_FALSE(solution.canConstruct("aa", "ab"));
+}
+
+TEST(RansomNote, Test3)
+{
+    Solution solution;
+    ASSERT_TRUE(solution.canConstruct("aa", "aab"));
 }

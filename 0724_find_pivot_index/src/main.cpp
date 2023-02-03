090c71a8c4b8fc2c2ac8e26a3fe2a2b1647e45e6
Gleb Dolgich <glebd70@gmail.com>
2023-02-03 20:23:50 +0000

    Optimise algorithm to avoid nested loops
    Use running sums as suggested by Daniil

--- 0724_find_pivot_index/src/main.cpp
+++ 0724_find_pivot_index/src/main.cpp
@@ -7,18 +7,25 @@ class Solution
 public:
     int pivotIndex(const std::vector<int>& nums)
     {
+        if (nums.empty())
+            return -1;
+
         const size_t size = nums.size();
+        int pivot_value = nums[0];
+        int left_sum = 0;
+        int right_sum = 0;
+        for (size_t i = 1; i < size; ++i)
+            right_sum += nums[i];
+
         for (size_t pivot = 0; pivot < size; ++pivot)
         {
-            int left_sum = 0;
-            int right_sum = 0;
-
-            for (size_t j = 0; j < pivot; ++j)
-                left_sum += nums[j];
-            for (size_t j = pivot + 1; j < size; ++j)
-                right_sum += nums[j];
             if (left_sum == right_sum)
                 return static_cast<int>(pivot);
+
+            left_sum += pivot_value;
+            if (pivot < size - 1)
+                pivot_value = nums[pivot + 1];
+            right_sum -= pivot_value;
         }
 
         return -1;
@@ -50,4 +57,11 @@ TEST(PivotIndex, TestEmpty)
 {
     Solution solution;
     ASSERT_EQ(solution.pivotIndex({}), -1);
+}
+
+TEST(PivotIndex, LastPivot)
+{
+    const std::vector<int> input = {1, -1, 0};
+    Solution solution;
+    ASSERT_EQ(solution.pivotIndex(input), 2);
 }

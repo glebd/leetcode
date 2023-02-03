cd9e16f90b55cfbf2caab1ea2738e9c423b368ec
Gleb Dolgich <glebd70@gmail.com>
2023-02-03 18:49:30 +0000

    Add tests

--- 0724_find_pivot_index/src/main.cpp
+++ 0724_find_pivot_index/src/main.cpp
@@ -1,9 +1,39 @@
-#include <iostream>
+#include <gtest/gtest.h>
 
-using namespace std;
+#include <vector>
 
-int main(int argc, char** argv)
+class Solution
 {
-    cout << "hello world!" << endl;
-    return 0;
+public:
+    int pivotIndex(const std::vector<int>& nums)
+    {
+        return -1;
+    }
+};
+
+TEST(PivotIndex, Test1)
+{
+    const std::vector<int> input = {1, 7, 3, 6, 5, 6};
+    Solution solution;
+    ASSERT_EQ(solution.pivotIndex(input), 3);
+}
+
+TEST(PivotIndex, Test2)
+{
+    const std::vector<int> input = {1, 2, 3};
+    Solution solution;
+    ASSERT_EQ(solution.pivotIndex(input), -1);
+}
+
+TEST(PivotIndex, Test3)
+{
+    const std::vector<int> input = {2, 1, -1};
+    Solution solution;
+    ASSERT_EQ(solution.pivotIndex(input), 0);
+}
+
+TEST(PivotIndex, TestEmpty)
+{
+    Solution solution;
+    ASSERT_EQ(solution.pivotIndex({}), -1);
 }

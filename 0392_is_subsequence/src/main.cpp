d9d2e39f00050e50def05640b1140e7a446e860c
Gleb Dolgich <glebd70@gmail.com>
2023-02-09 20:32:38 +0000

    Add empty solution template

--- 0392_is_subsequence/src/main.cpp
+++ 0392_is_subsequence/src/main.cpp
@@ -1,5 +1,17 @@
 #include <gtest/gtest.h>
 
+#include <string>
+
+// https://leetcode.com/problems/is-subsequence/?envType=study-plan&id=level-1
+class Solution
+{
+public:
+    static bool isSubsequence(const std::string& s, const std::string& t)
+    {
+        return false;
+    }
+};
+
 TEST(IsSubsequence, Test1)
 {
     ASSERT_TRUE(true);

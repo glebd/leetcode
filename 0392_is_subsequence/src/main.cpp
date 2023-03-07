8c6a0955bee87d30afcf55cac44bd236122cdb21
Gleb Dolgich <glebd70@gmail.com>
2023-03-07 20:19:34 +0000

    Use hash map
    Beats 100% (speed), 9% (mem)

--- 0392_is_subsequence/src/main.cpp
+++ 0392_is_subsequence/src/main.cpp
@@ -1,6 +1,7 @@
 #include <gtest/gtest.h>
 
 #include <string>
+#include <unordered_map>
 
 // https://leetcode.com/problems/is-subsequence/?envType=study-plan&id=level-1
 class Solution
@@ -8,22 +9,45 @@ class Solution
 public:
     static bool isSubsequence(const std::string& s, const std::string& t)
     {
-        auto leftBound = s.length();
-        auto rightBound = t.length();
-        size_t left = 0;
-        size_t right = 0;
-        while (left < leftBound && right < rightBound)
+        std::unordered_map<char, std::vector<int>> letter_indexes;
+        for (size_t i = 0; i < t.length(); ++i)
         {
-            if (s[left] == t[right])
+            auto found = letter_indexes.find(t[i]);
+            if (found == letter_indexes.end())
             {
-                ++left, ++right;
+                letter_indexes.insert({t[i], {static_cast<int>(i)}});
             }
             else
             {
-                ++right;
+                auto& indexes = found->second;
+                indexes.push_back(static_cast<int>(i));
             }
         }
-        return left == leftBound;
+
+        int curr_match_index = -1;
+        for (const char ch : s)
+        {
+            auto found = letter_indexes.find(ch);
+            if (found == letter_indexes.end())
+                return false;
+
+            bool matched = false;
+            const auto& indexes = found->second;
+            for (const int index : indexes)
+            {
+                if (curr_match_index < index)
+                {
+                    curr_match_index = index;
+                    matched = true;
+                    break;
+                }
+            }
+
+            if (!matched)
+                return false;
+        }
+
+        return true;
     }
 };
 

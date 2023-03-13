3a07396e89340ab7a879d7f8696abf360eb3b3a6
Gleb Dolgich <glebd70@gmail.com>
2023-03-13 21:40:52 +0000

    Failing implementation

--- 0021_merge_two_sorted_lists/src/main.cpp
+++ 0021_merge_two_sorted_lists/src/main.cpp
@@ -1,6 +1,7 @@
 #include <gtest/gtest.h>
 
 #include <memory>
+#include <vector>
 
 struct ListNode
 {
@@ -25,12 +26,52 @@ class Solution
 public:
     static ListNode* mergeTwoLists(ListNode* list1, ListNode* list2)
     {
-        return nullptr;
+        if (list1 == nullptr)
+            return list2;
+        if (list2 == nullptr)
+            return list1;
+        ListNode* merged = nullptr;
+        ListNode* cur_merged = nullptr;
+        ListNode* cur_other = nullptr;
+        ListNode* cur1 = list1;
+        ListNode* cur2 = list2;
+        if (cur2->val >= cur1->val)
+        {
+            cur_merged = cur1;
+            cur_other = cur2;
+        }
+        else
+        {
+            cur_merged = cur2;
+            cur_other = cur1;
+        }
+        merged = cur_merged;
+        while (cur_other != nullptr)
+        {
+            ListNode* old_cur_merged = cur_merged;
+            while (cur_merged->val <= cur_other->val)
+            {
+                if (cur_merged->next == nullptr)
+                {
+                    cur_merged->next = cur_other;
+                    return merged;
+                }
+                old_cur_merged = cur_merged;
+                cur_merged = cur_merged->next;
+            }
+            old_cur_merged->next = cur_other;
+            ListNode* new_cur_other = cur_other->next;
+            cur_other->next = cur_merged;
+            cur_other = new_cur_other;
+        }
+        return merged;
     }
 };
 
 TEST(MergeSortedLists, Test1)
 {
+    using namespace testing;
+
     auto list1node1 = std::make_unique<ListNode>(1);
     auto list1node2 = std::make_unique<ListNode>(2);
     auto list1node3 = std::make_unique<ListNode>(4);
@@ -45,9 +86,18 @@ TEST(MergeSortedLists, Test1)
     list2node1->next = list2node2.get();
     list2node2->next = list2node3.get();
 
+    std::vector<int> expected = {1,1,2,3,4,4};
     auto* merged = Solution::mergeTwoLists(list1, list2);
 
     ASSERT_TRUE(merged != nullptr);
+    std::vector<int> actual;
+    while (merged)
+    {
+        actual.push_back(merged->val);
+        merged = merged->next;
+    }
+    ASSERT_EQ(actual, expected);
+    /*
     ASSERT_EQ(merged, list1node1.get());
     ASSERT_TRUE(merged->next != nullptr);
     ASSERT_EQ(merged->next, list2node1.get());
@@ -59,6 +109,7 @@ TEST(MergeSortedLists, Test1)
     ASSERT_EQ(merged->next->next->next->next, list1node3.get());
     ASSERT_TRUE(merged->next->next->next->next->next != nullptr);
     ASSERT_EQ(merged->next->next->next->next->next, list2node3.get());
+     */
 }
 
 TEST(MergeSortedLists, TestEmpty)
@@ -81,4 +132,35 @@ TEST(MergeSortedLists, TestSecondEmpty)
     auto* merged = Solution::mergeTwoLists(node.get(), nullptr);
     ASSERT_NE(merged, nullptr);
     ASSERT_EQ(merged, node.get());
+}
+
+TEST(MergeSortedLists, Fail1)
+{
+    // ERROR: time limit exceeded
+    //[-10,-9,-6,-4,1,9,9]
+    auto list1node7 = std::make_unique<ListNode>(-10);
+    auto list1node6 = std::make_unique<ListNode>(-9, list1node7.get());
+    auto list1node5 = std::make_unique<ListNode>(-6, list1node6.get());
+    auto list1node4 = std::make_unique<ListNode>(-4, list1node5.get());
+    auto list1node3 = std::make_unique<ListNode>(1, list1node4.get());
+    auto list1node2 = std::make_unique<ListNode>(9, list1node3.get());
+    auto list1node1 = std::make_unique<ListNode>(9, list1node2.get());
+    //[-5,-3,0,7,8,8]
+    auto list2node6 = std::make_unique<ListNode>(-5);
+    auto list2node5 = std::make_unique<ListNode>(-3, list2node6.get());
+    auto list2node4 = std::make_unique<ListNode>(0, list2node5.get());
+    auto list2node3 = std::make_unique<ListNode>(7, list2node4.get());
+    auto list2node2 = std::make_unique<ListNode>(8, list2node3.get());
+    auto list2node1 = std::make_unique<ListNode>(8, list2node2.get());
+
+    std::vector<int> expected = {-10,-9,-6,-5,-4,-3,0,1,7,8,8,9,9};
+    auto* merged = Solution::mergeTwoLists(list1node1.get(), list2node1.get());
+    ASSERT_NE(merged, nullptr);
+    std::vector<int> actual;
+    while (merged)
+    {
+        actual.push_back(merged->val);
+        merged = merged->next;
+    }
+    ASSERT_EQ(actual, expected);
 }

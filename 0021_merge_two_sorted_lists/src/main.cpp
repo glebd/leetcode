dfbf0d84543e8d5b8003e83dcb81407a2a529cac
Gleb Dolgich <glebd70@gmail.com>
2023-03-13 18:21:00 +0000

    Copy solution template

--- 0021_merge_two_sorted_lists/src/main.cpp
+++ 0021_merge_two_sorted_lists/src/main.cpp
@@ -1,5 +1,32 @@
 #include <gtest/gtest.h>
 
+struct ListNode
+{
+    int val;
+    ListNode* next;
+    ListNode()
+        : val(0), next(nullptr)
+    {
+    }
+    ListNode(int x)
+        : val(x), next(nullptr)
+    {
+    }
+    ListNode(int x, ListNode* next)
+        : val(x), next(next)
+    {
+    }
+};
+
+class Solution
+{
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2)
+    {
+        return nullptr;
+    }
+};
+
 TEST(MergeSortedLists, Test1)
 {
     ASSERT_TRUE(true);

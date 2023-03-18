5738ad9f29b6ac368887706f1f63e03d62da24f7
Gleb Dolgich <glebd70@gmail.com>
2023-03-18 13:58:25 +0000

    add solution template

--- 0206_reverse_linked_list/src/main.cpp
+++ 0206_reverse_linked_list/src/main.cpp
@@ -1,5 +1,31 @@
 #include <gtest/gtest.h>
 
+struct ListNode
+{
+    int val;
+    ListNode* next;
+    ListNode()
+        : val(0), next(nullptr)
+    {
+    }
+    ListNode(int x)
+        : val(x), next(nullptr)
+    {
+    }
+    ListNode(int x, ListNode* next)
+        : val(x), next(next)
+    {
+    }
+};
+
+class Solution
+{
+public:
+    static ListNode* reverseList(ListNode* head)
+    {
+    }
+};
+
 TEST(ReverseLinkedList, Test1)
 {
     ASSERT_TRUE(true);

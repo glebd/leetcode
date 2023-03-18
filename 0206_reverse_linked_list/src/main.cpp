d30793e69e6d62be47579d7b110b4533e64c90fb
Gleb Dolgich <glebd70@gmail.com>
2023-03-18 14:00:11 +0000

    add helper functions

--- 0206_reverse_linked_list/src/main.cpp
+++ 0206_reverse_linked_list/src/main.cpp
@@ -1,5 +1,9 @@
 #include <gtest/gtest.h>
 
+#include <memory>
+#include <vector>
+#include <algorithm>
+
 struct ListNode
 {
     int val;
@@ -18,11 +22,33 @@ struct ListNode
     }
 };
 
+auto values2list(const std::vector<int>& values)
+{
+    std::vector<std::unique_ptr<ListNode>> list1(values.size());
+    std::transform(values.begin(), values.end(), list1.begin(), [](const int value)
+                   { return std::make_unique<ListNode>(value); });
+    for (int i = 0; i < static_cast<int>(list1.size()) - 1; ++i)
+        list1[i]->next = list1[i + 1].get();
+    return list1;
+}
+
+auto list2values(ListNode* head)
+{
+    std::vector<int> values;
+    while (head)
+    {
+        values.push_back(head->val);
+        head = head->next;
+    }
+    return values;
+}
+
 class Solution
 {
 public:
     static ListNode* reverseList(ListNode* head)
     {
+        return nullptr;
     }
 };
 

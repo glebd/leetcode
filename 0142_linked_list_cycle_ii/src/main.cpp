c59135022d1703a0c85ee24ea19569c9d2350810
Gleb Dolgich <glebd70@gmail.com>
2023-04-01 14:02:22 +0100

    add helper functions and ListNode definition

--- 0142_linked_list_cycle_ii/src/main.cpp
+++ 0142_linked_list_cycle_ii/src/main.cpp
@@ -1,4 +1,47 @@
+#include <algorithm>
 #include <gtest/gtest.h>
+#include <memory>
+#include <vector>
+
+struct ListNode
+{
+    int val;
+    ListNode* next;
+    ListNode()
+        : val(0), next(nullptr)
+    {
+    }
+    ListNode(int x)
+        : val(x), next(nullptr)
+    {
+    }
+    ListNode(int x, ListNode* next)
+        : val(x), next(next)
+    {
+    }
+};
+
+auto values2list(const std::vector<int>& values)
+{
+    std::vector<std::unique_ptr<ListNode>> list1(values.size());
+    std::transform(values.begin(), values.end(), list1.begin(), [](const int value)
+                   { return std::make_unique<ListNode>(value); });
+    for (int i = 0; i < static_cast<int>(list1.size()) - 1; ++i)
+        list1[i]->next = list1[i + 1].get();
+    return list1;
+}
+
+auto list2values(ListNode* head)
+{
+    std::vector<int> values;
+    while (head)
+    {
+        values.push_back(head->val);
+        head = head->next;
+    }
+    return values;
+}
+
 
 TEST(LinkedListCycle2, Test1)
 {

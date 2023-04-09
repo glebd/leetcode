d323240d5a3c8e9c11b9e2638f8470e451d927d8
Gleb Dolgich <glebd70@gmail.com>
2023-04-09 23:29:45 +0100

    add Node definition from problem

--- 0589_nary_tree_preorder_traversal/src/main.cpp
+++ 0589_nary_tree_preorder_traversal/src/main.cpp
@@ -1,5 +1,27 @@
 #include <gtest/gtest.h>
 
+#include <vector>
+
+// Definition for a Node.
+class Node
+{
+public:
+    int val{};
+    std::vector<Node*> children{};
+
+    Node() = default;
+
+    explicit Node(int _val)
+        : val(_val)
+    {
+    }
+
+    Node(int _val, std::vector<Node*> _children)
+        : val(_val), children(std::move(_children))
+    {
+    }
+};
+
 TEST(Tree, Test1)
 {
     ASSERT_TRUE(true);

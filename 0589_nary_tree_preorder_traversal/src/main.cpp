f3d2a59976476a7829e34d6df129fe8d7b1275ab
Gleb Dolgich <glebd70@gmail.com>
2023-04-09 23:41:31 +0100

    add problem tests with data

--- 0589_nary_tree_preorder_traversal/src/main.cpp
+++ 0589_nary_tree_preorder_traversal/src/main.cpp
@@ -31,5 +31,18 @@ public:
 
 TEST(Tree, Test1)
 {
-    ASSERT_TRUE(true);
+    // -1 means <null> in test data, as node value is 0 <= val <= 10^4
+    std::vector<int> tree{1, -1, 3, 2, 4, -1, 5, 6};
+    std::vector<int> expected{1, 3, 5, 6, 2, 4};
+    auto actual = Solution::preorder(nullptr);
+    ASSERT_EQ(actual, expected);
+}
+
+TEST(Tree, Test2)
+{
+    // -1 means <null> in test data, as node value is 0 <= val <= 10^4
+    std::vector<int> tree{1, -1, 2, 3, 4, 5, -1, -1, 6, 7, -1, 8, -1, 9, 10, -1, -1, 11, -1, 12, -1, 13, -1, -1, 14};
+    std::vector<int> expected{1, 2, 3, 6, 7, 11, 14, 4, 8, 12, 5, 9, 13, 10};
+    auto actual = Solution::preorder(nullptr);
+    ASSERT_EQ(actual, expected);
 }

0056164f7d6ad6a3a61fcbdab0b360cc75022d7a
Gleb Dolgich <glebd70@gmail.com>
2023-04-09 20:24:55 +0100

    add gtest

--- 0589_nary_tree_preorder_traversal/xmake.lua
+++ 0589_nary_tree_preorder_traversal/xmake.lua
@@ -1,8 +1,12 @@
 add_rules("mode.debug", "mode.release")
+set_languages("c11", "c++17")
+add_requires("gtest")
+set_optimize("none")
 
 target("0589_nary_tree_preorder_traversal")
     set_kind("binary")
     add_files("src/*.cpp")
+    add_packages("gtest")
 
 --
 -- If you want to known more usage about xmake, please see https://xmake.io

5165a8185aef68b2c3f292edfe642d1e85ab94e6
Gleb Dolgich <glebd70@gmail.com>
2023-04-14 19:46:23 +0100

    add test helper function

--- 0589_nary_tree_preorder_traversal/src/main.cpp
+++ 0589_nary_tree_preorder_traversal/src/main.cpp
@@ -45,6 +45,53 @@ auto value2node(const int value)
     return std::make_unique<Node>(value);
 }
 
+auto values2nodes(const std::vector<int>& values)
+{
+    std::vector<std::vector<std::unique_ptr<Node>>> all_nodes;
+    int level = 0;// tree level we're currently processing
+    std::vector<std::unique_ptr<Node>> cur_level_nodes;
+    int parent_index = -1;// index of up-level node to add children to
+    for (const int value: values)
+    {
+        // 1st value of -1 increments depth, but only if the current level is not empty
+        // otherwise it skips to the next up-level node's children
+        if (value != -1 /*&& !cur_level_nodes.empty()*/)
+        {
+            // add node with this value to current level nodes
+            cur_level_nodes.emplace_back(value2node(value));
+            if (parent_index > -1)
+            {
+                auto& parent_node = all_nodes[level - 1][parent_index];
+                auto& node = cur_level_nodes.back();
+                parent_node->children.push_back(node.get());
+            }
+        }
+        else
+        {
+            // check that we're at level 0
+            // check that we haven't run out of parent nodes
+            ++parent_index;
+            if (level == 0 || parent_index >= all_nodes[level - 1].size())// ran out of parents
+            {
+                // add current level nodes to all nodes
+                all_nodes.emplace_back(std::move(cur_level_nodes));
+                // increment current level
+                ++level;
+                // clear current level nodes
+                cur_level_nodes.clear();
+                parent_index = 0;
+            }
+        }
+    }
+
+    if (!cur_level_nodes.empty())
+    {
+        all_nodes.emplace_back(std::move(cur_level_nodes));
+    }
+
+    return all_nodes;
+}
+
 //
 // *** TESTS ***
 //
@@ -67,6 +114,7 @@ TEST(Tree, Test1)
 
     // -1 means <null> in test data, as node value is 0 <= val <= 10^4
     std::vector<int> tree{1, -1, 3, 2, 4, -1, 5, 6};
+    auto nodes = values2nodes(tree);
     std::vector<int> expected{1, 3, 5, 6, 2, 4};
     auto actual = Solution::preorder(nullptr);
     ASSERT_EQ(actual, expected);

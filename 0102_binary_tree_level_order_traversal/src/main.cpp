c649422cfe589fa87161f450c8232785932826c6
Gleb Dolgich <glebd70@gmail.com>
2023-04-23 13:55:22 +0100

    Add boilerplate code from leetcode

--- 0102_binary_tree_level_order_traversal/src/main.cpp
+++ 0102_binary_tree_level_order_traversal/src/main.cpp
@@ -1,5 +1,31 @@
 #include <gtest/gtest.h>
 
+#include <vector>
+
+// Definition for a binary tree node.
+struct TreeNode
+{
+    int val{0};
+    TreeNode* left{};
+    TreeNode* right{};
+    TreeNode() = default;
+    explicit TreeNode(int x) : val(x)
+    {
+    }
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right)
+    {
+    }
+};
+
+class Solution
+{
+public:
+    std::vector<std::vector<int>> levelOrder(TreeNode* root)
+    {
+        return {};
+    }
+};
+
 TEST(BinTreeTraversal, Test1)
 {
     ASSERT_TRUE(true);

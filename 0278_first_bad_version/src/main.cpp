efa06f8fb25608282ac696ba87ac1c0d49976ef3
Gleb Dolgich <glebd70@gmail.com>
2023-05-02 19:19:35 +0100

    Mock isBadVersion() using lambda

--- 0278_first_bad_version/src/main.cpp
+++ 0278_first_bad_version/src/main.cpp
@@ -1,18 +1,22 @@
+#include <functional>
 #include <gtest/gtest.h>
 
 // https://leetcode.com/problems/first-bad-version/
 
 // The API isBadVersion is defined for you.
-bool isBadVersion(int version)
-{
-    return false;
-}
+
+//bool isBadVersion(int version)
+//{
+//    return false;
+//}
 
 class Solution
 {
 public:
-    static int firstBadVersion(int n)
+    static int firstBadVersion(int n, const std::function<bool(int)>& isBadVersion)
     {
+        if (isBadVersion(n))
+            return n;
         return 0;
     }
 };
@@ -21,12 +25,12 @@ public:
 // Output: 4
 TEST(FirstBadVersion, Test1)
 {
-    ASSERT_EQ(Solution::firstBadVersion(5), 4);
+    ASSERT_EQ(Solution::firstBadVersion(5, [](int n) { return n >= 4; }), 4);
 }
 
 // Input: n = 1, bad = 1
 // Output: 1
 TEST(FirstBadVersion, Test2)
 {
-    ASSERT_EQ(Solution::firstBadVersion(1), 1);
+    ASSERT_EQ(Solution::firstBadVersion(1, [](int n) { return n >= 1; }), 1);
 }

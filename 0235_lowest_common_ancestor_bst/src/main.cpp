9365c85817988a2cdac7a444500b73cd64dfdae4
Gleb Dolgich <glebd70@gmail.com>
2023-05-24 17:59:35 +0100

    Add skeleton solution and failing tests

--- 0235_lowest_common_ancestor_bst/src/main.cpp
+++ 0235_lowest_common_ancestor_bst/src/main.cpp
@@ -1,7 +1,47 @@
 #include <gtest/gtest.h>
 #include <tools/binary_tree.h>
 
+class Solution
+{
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q)
+    {
+        return nullptr;
+    }
+};
+
 TEST(CommonAncestor, Test1)
 {
-    ASSERT_TRUE(true);
+    using namespace binary_tree;
+    std::vector<int> values = {6, 2, 8, 0, 4, 7, 9, -1, -1, 3, 5};
+    auto nodes = values2nodes(values);
+    TreeNode* root = nodes2root(nodes);
+    Solution solution;
+    TreeNode* p = root->left;
+    TreeNode* q = root->right;
+    ASSERT_EQ(solution.lowestCommonAncestor(root, p, q), root);
+}
+
+TEST(CommonAncestor, Test2)
+{
+    using namespace binary_tree;
+    std::vector<int> values = {6, 2, 8, 0, 4, 7, 9, -1, -1, 3, 5};
+    auto nodes = values2nodes(values);
+    TreeNode* root = nodes2root(nodes);
+    Solution solution;
+    TreeNode* p = root->left;
+    TreeNode* q = p->right;
+    ASSERT_EQ(solution.lowestCommonAncestor(root, p, q), p);
+}
+
+TEST(CommonAncestor, Test3)
+{
+    using namespace binary_tree;
+    std::vector<int> values = {2, 1};
+    auto nodes = values2nodes(values);
+    TreeNode* root = nodes2root(nodes);
+    Solution solution;
+    TreeNode* p = root;
+    TreeNode* q = p->left;
+    ASSERT_EQ(solution.lowestCommonAncestor(root, p, q), root);
 }

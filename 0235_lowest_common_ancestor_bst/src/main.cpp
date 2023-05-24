f4ecbf0d125e00653ba88e013bdc5e181d718e65
Gleb Dolgich <glebd70@gmail.com>
2023-05-24 19:05:02 +0100

    Add first solution

--- 0235_lowest_common_ancestor_bst/src/main.cpp
+++ 0235_lowest_common_ancestor_bst/src/main.cpp
@@ -1,14 +1,44 @@
 #include <gtest/gtest.h>
 #include <tools/binary_tree.h>
+#include <vector>
 
 // https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
 
 class Solution
 {
 public:
+    /// Find path to the node in BST
+    /// \param root BST root node
+    /// \param node Node to search for
+    /// \return Vector of nodes from root (incl.) to the node (incl.)
+    std::vector<TreeNode*> findPath(TreeNode* root, TreeNode* node)
+    {
+        std::vector<TreeNode*> path;
+        TreeNode* cur = root;
+        do {
+            path.push_back(cur);
+            if (node == root)
+                return path;
+            if (node->val < cur->val)
+                cur = cur->left;
+            else
+                cur = cur->right;
+        } while (cur->val != node->val);
+        path.push_back(cur);
+        return path;
+    }
+
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q)
     {
-        return nullptr;
+        auto p_path = findPath(root, p);
+        auto q_path = findPath(root, q);
+        size_t index = 0;
+        const size_t max_size = std::min(p_path.size(), q_path.size());
+        while (index < max_size && p_path[index] == q_path[index])
+        {
+            ++index;
+        }
+        return p_path[index - 1]; // last matching index
     }
 };
 

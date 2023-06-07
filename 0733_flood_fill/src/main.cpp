3b15c4897bc637f023e524e8433e153c8c291e30
Gleb Dolgich <glebd70@gmail.com>
2023-06-07 18:51:46 +0100

    Add recursive solution

--- 0733_flood_fill/src/main.cpp
+++ 0733_flood_fill/src/main.cpp
@@ -7,7 +7,36 @@ class Solution
 public:
     static std::vector<std::vector<int>> floodFill(std::vector<std::vector<int>>& image, int sr, int sc, int color)
     {
-        return {{}};
+        int& center = image[sr][sc];
+        if (center == color)
+            return image;
+        const int old_center = center;
+        center = color;
+        if (sr > 0)
+        {
+            int& top = image[sr - 1][sc];
+            if (top == old_center)
+                floodFill(image, sr - 1, sc, color);
+        }
+        if (sc < static_cast<int>(image[sr].size()) - 1)
+        {
+            int& right = image[sr][sc + 1];
+            if (right == old_center)
+                floodFill(image, sr, sc + 1, color);
+        }
+        if (sr < static_cast<int>(image.size()) - 1)
+        {
+            int& bottom = image[sr + 1][sc];
+            if (bottom == old_center)
+                floodFill(image, sr + 1, sc, color);
+        }
+        if (sc > 0)
+        {
+            int& left = image[sr][sc - 1];
+            if (left == old_center)
+                floodFill(image, sr, sc - 1, color);
+        }
+        return image;
     }
 };
 

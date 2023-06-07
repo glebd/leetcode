2b0c298ee1f72af0ec1a2c880aa3c12f14468089
Gleb Dolgich <glebd70@gmail.com>
2023-06-07 18:17:55 +0100

    Add failing tests

--- 0733_flood_fill/src/main.cpp
+++ 0733_flood_fill/src/main.cpp
@@ -1,6 +1,28 @@
 #include <gtest/gtest.h>
 
+#include <vector>
+
+class Solution
+{
+public:
+    static std::vector<std::vector<int>> floodFill(std::vector<std::vector<int>>& image, int sr, int sc, int color)
+    {
+        return {{}};
+    }
+};
+
 TEST(FloodFill, Test1)
 {
-    ASSERT_TRUE(true);
+    std::vector<std::vector<int>> image = {{1, 1, 1}, {1, 1, 0}, {1, 0, 1}};
+    const std::vector<std::vector<int>> expected = {{2, 2, 2}, {2, 2, 0}, {2, 0, 1}};
+    const auto actual = Solution::floodFill(image, 1, 1, 2);
+    ASSERT_EQ(actual, expected);
+}
+
+TEST(FloodFill, Test2)
+{
+    std::vector<std::vector<int>> image = {{0, 0, 0}, {0, 0, 0}};
+    const std::vector<std::vector<int>> expected = {{0, 0, 0}, {0, 0, 0}};
+    const auto actual = Solution::floodFill(image, 0, 0, 0);
+    ASSERT_EQ(actual, expected);
 }

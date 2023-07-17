846af13a6d16fb125cb3fc3438ac5eac74628b29
Gleb Dolgich <glebd70@gmail.com>
2023-07-17 20:16:26 +0100

    Implement DFS solution

--- 0200_number_of_islands/src/main.cpp
+++ 0200_number_of_islands/src/main.cpp
@@ -10,19 +10,8 @@ struct Coordinate
     int n{};
 };
 
-bool operator<(const Coordinate& lhs, const Coordinate& rhs)
-{
-    return std::tie(lhs.m, lhs.n) < std::tie(rhs.m, rhs.n);
-}
-
-struct Island
-{
-    std::set<Coordinate> coordinates;
-};
-
 class Solution
 {
-    std::set<Coordinate> checked_coordinates;
     int num_islands{0};
 
 public:
@@ -35,55 +24,43 @@ public:
                 Coordinate c{m, n};
                 char ch = grid[c.m][c.n];
                 if (ch == '1')
-                    find_island(0, c, grid);
+                {
+                    ++num_islands;
+                    find_island(c, grid);
+                }
             }
         }
         return num_islands;
     }
 
-    void find_island(int level, const Coordinate& coord, std::vector<std::vector<char>>& grid)
+    void find_island(const Coordinate& coord, std::vector<std::vector<char>>& grid)
     {
-        if (checked_coordinates.find(coord) != checked_coordinates.end())
-            return;
-        checked_coordinates.insert(coord);
         char& ch = grid[coord.m][coord.n];
-        if (ch == '1')
-        {
-            if (coord.m > 0)
-            {
-                char ch_top = grid[coord.m - 1][coord.n];
-                if (ch_top == '1')
-                    find_island(level + 1, {coord.m - 1, coord.n}, grid);
-            }
-            if (coord.n < static_cast<int>(grid[coord.m].size()) - 1)
-            {
-                char ch_right = grid[coord.m][coord.n + 1];
-                if (ch_right == '1')
-                    find_island(level + 1, {coord.m, coord.n + 1}, grid);
-            }
-            if (coord.m < static_cast<int>(grid.size() - 1))
-            {
-                char ch_bottom = grid[coord.m + 1][coord.n];
-                if (ch_bottom == '1')
-                    find_island(level + 1, {coord.m + 1, coord.n}, grid);
-            }
-            if (coord.n > 0)
-            {
-                char ch_left = grid[coord.m][coord.n - 1];
-                if (ch_left == '1')
-                    find_island(level + 1, {coord.m, coord.n - 1}, grid);
-            }
-            ch = '3'; // was '1'
-        }
-        else if (ch == '0')
-        {
-            ch = '2'; // was '0'
-        }
+        ch = '0';
 
-        if (level == 0)
+        if (coord.m > 0)
         {
-            // add island
-            ++num_islands;
+            char ch_top = grid[coord.m - 1][coord.n];
+            if (ch_top == '1')
+                find_island({coord.m - 1, coord.n}, grid);
+        }
+        if (coord.n < static_cast<int>(grid[coord.m].size()) - 1)
+        {
+            char ch_right = grid[coord.m][coord.n + 1];
+            if (ch_right == '1')
+                find_island({coord.m, coord.n + 1}, grid);
+        }
+        if (coord.m < static_cast<int>(grid.size() - 1))
+        {
+            char ch_bottom = grid[coord.m + 1][coord.n];
+            if (ch_bottom == '1')
+                find_island({coord.m + 1, coord.n}, grid);
+        }
+        if (coord.n > 0)
+        {
+            char ch_left = grid[coord.m][coord.n - 1];
+            if (ch_left == '1')
+                find_island({coord.m, coord.n - 1}, grid);
         }
     }
 };

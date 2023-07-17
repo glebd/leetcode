c146addaf1f151cb295ba6ab49773be14a755296
Gleb Dolgich <glebd70@gmail.com>
2023-07-17 17:55:59 +0100

    Add skeleton solution and failing tests

--- 0200_number_of_islands/src/main.cpp
+++ 0200_number_of_islands/src/main.cpp
@@ -1,6 +1,43 @@
+#include <vector>
+
 #include <gtest/gtest.h>
+
+class Solution
+{
+public:
+    static int numIslands(std::vector<std::vector<char>>& grid)
+    {
+        return 0;
+    }
+};
 
 TEST(NumberOfIslands, Test1)
 {
-    ASSERT_TRUE(true);
+    std::vector<std::vector<char>> grid = {
+        {'1','1','1','1','0'},
+        {'1','1','0','1','0'},
+        {'1','1','0','0','0'},
+        {'0','0','0','0','0'}
+    };
+    int actual = Solution::numIslands(grid);
+    ASSERT_EQ(actual, 1);
+}
+
+TEST(NumberOfIslands, Test2)
+{
+    std::vector<std::vector<char>> grid = {
+        {'1','1','0','0','0'},
+        {'1','1','0','0','0'},
+        {'0','0','1','0','0'},
+        {'0','0','0','1','1'}
+    };
+    int actual = Solution::numIslands(grid);
+    ASSERT_EQ(actual, 3);
+}
+
+TEST(NumberOfIslands, TestEmpty)
+{
+    std::vector<std::vector<char>> grid = {{}};
+    int actual = Solution::numIslands(grid);
+    ASSERT_EQ(actual, 0);
 }

ecbf8ae58a47d78e1fca1495af6a5d782f3f051c
Gleb Dolgich <glebd70@gmail.com>
2024-05-24 17:57:00 +0100

    Add skeleton solution and problem link

--- 1768_merge_strings_alternately/main.cpp
+++ 1768_merge_strings_alternately/main.cpp
@@ -1,4 +1,16 @@
+#include <string>
+
 #include <gtest/gtest.h>
+
+// https://leetcode.com/problems/merge-strings-alternately/description/
+
+class Solution
+{
+public:
+  std::string mergeAlternately(std::string word1, std::string word2)
+  {
+  }
+};
 
 TEST(MergeStringsAlt, Test1)
 {

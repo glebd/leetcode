891e08f342c990685caa58810196b4b9a5acee32
Gleb Dolgich <glebd70@gmail.com>
2024-05-25 21:46:02 +0100

    Add failing tests

--- 1768_merge_strings_alternately/main.cpp
+++ 1768_merge_strings_alternately/main.cpp
@@ -7,12 +7,38 @@
 class Solution
 {
 public:
-  std::string mergeAlternately(std::string word1, std::string word2)
+  static std::string mergeAlternately(std::string word1, std::string word2)
   {
+    return {};
   }
 };
 
+TEST(MergeStringsAlt, Test00)
+{
+  ASSERT_EQ(Solution::mergeAlternately("", ""), "");
+}
+
+TEST(MergeStringsAlt, Test01)
+{
+  ASSERT_EQ(Solution::mergeAlternately("abc", ""), "abc");
+}
+
+TEST(MergeStringsAlt, Test02)
+{
+  ASSERT_EQ(Solution::mergeAlternately("", "pqr"), "pqr");
+}
+
 TEST(MergeStringsAlt, Test1)
 {
-  ASSERT_TRUE(true);
+  ASSERT_EQ(Solution::mergeAlternately("abc", "pqr"), "apbqcr");
+}
+
+TEST(MergeStringsAlt, Test2)
+{
+  ASSERT_EQ(Solution::mergeAlternately("ab", "pqrs"), "apbqrs");
+}
+
+TEST(MergeStringsAlt, Test3)
+{
+  ASSERT_EQ(Solution::mergeAlternately("abcd", "pq"), "apbqcd");
 }

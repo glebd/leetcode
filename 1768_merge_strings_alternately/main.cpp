628b203897866018a12e7b4da310754cdc68ccef
Gleb Dolgich <glebd70@gmail.com>
2024-05-26 00:49:44 +0100

    Use C-style pointers

--- 1768_merge_strings_alternately/main.cpp
+++ 1768_merge_strings_alternately/main.cpp
@@ -11,20 +11,21 @@ public:
   static std::string mergeAlternately(std::string word1, std::string word2)
   {
     std::string result;
-    std::vector word_ps{&word1, &word2};
+    const char* p1 = word1.c_str();
+    const char* p2 = word2.c_str();
+    std::vector word_ps{p1, p2};
     int index = 0;
-    while (!word1.empty() && !word2.empty()) {
-      std::string* word_p = word_ps[index];
-      if (!word_p->empty()) {
-        result += (*word_p)[0];
-        *word_p = &(*word_p)[1];
+    while (*word_ps[0] && *word_ps[1]) {
+      const char*& word_p = word_ps[index];
+      if (*word_p) {
+        result += *word_p++;
         index = 1 - index;
       }
     }
-    if (!word1.empty())
-      result += word1;
-    else if (!word2.empty())
-      result += word2;
+    if (*word_ps[0])
+      result += word_ps[0];
+    else if (*word_ps[1])
+      result += word_ps[1];
     return result;
   }
 };

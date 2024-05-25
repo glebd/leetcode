349f244c213d763beefca0f58de6090cfbe1cec4
Gleb Dolgich <glebd70@gmail.com>
2024-05-25 22:44:53 +0100

    Implement trivial solution
    Beats 26% (speed), 78% (memory).

--- 1768_merge_strings_alternately/main.cpp
+++ 1768_merge_strings_alternately/main.cpp
@@ -9,7 +9,24 @@ class Solution
 public:
   static std::string mergeAlternately(std::string word1, std::string word2)
   {
-    return {};
+    std::string result;
+    int index = 1;
+    while (word1.length() && word2.length()) {
+      if (index == 1 && word1.length()) {
+        result += word1[0];
+        word1 = &word1[1];
+        index = 2;
+      } else if (index == 2 && word2.length()) {
+        result += word2[0];
+        word2 = &word2[1];
+        index = 1;
+      }
+    }
+    if (word1.length())
+      result += word1;
+    else if (word2.length())
+      result += word2;
+    return result;
   }
 };
 

e20bd966933f89ff859c5d14d433fe9cc3c63377
Gleb Dolgich <glebd70@gmail.com>
2024-07-02 23:24:01 +0100

    Add problem link on LeetCode

--- tut_squares_of_sorted_array/main.cpp
+++ tut_squares_of_sorted_array/main.cpp
@@ -2,6 +2,8 @@
 
 #include <gtest/gtest.h>
 
+// https://leetcode.com/explore/interview/card/leetcodes-interview-crash-course-data-structures-and-algorithms/703/arraystrings/4689/
+
 class Solution
 {
 public:

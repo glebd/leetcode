3762f4584c0ee698cc281139b959a3764b740189
Gleb Dolgich <glebd70@gmail.com>
2024-07-02 23:03:59 +0100

    Add skeleton solution and tests

--- tut_squares_of_sorted_array/main.cpp
+++ tut_squares_of_sorted_array/main.cpp
@@ -1,6 +1,36 @@
+#include <vector>
+
 #include <gtest/gtest.h>
+
+class Solution
+{
+public:
+  static std::vector<int> sortedSquares(std::vector<int>& nums)
+  {
+    return nums;
+  }
+};
 
 TEST(SquaresOfSortedArray, Test1)
 {
-  ASSERT_TRUE(true);
+  std::vector<int> nums = {-4, -1, 0, 3, 10};
+  std::vector<int> expected = {0, 1, 9, 16, 100};
+  auto const actual = Solution::sortedSquares(nums);
+  ASSERT_EQ(actual, expected);
+}
+
+TEST(SquaresOfSortedArray, Test2)
+{
+  std::vector<int> nums = {-7, -3, 2, 3, 11};
+  std::vector<int> expected = {4, 9, 9, 49, 121};
+  auto const actual = Solution::sortedSquares(nums);
+  ASSERT_EQ(actual, expected);
+}
+
+TEST(SquaresOfSortedArray, Test3)
+{
+  std::vector<int> nums = {1};
+  std::vector<int> expected = {1};
+  auto const actual = Solution::sortedSquares(nums);
+  ASSERT_EQ(actual, expected);
 }

5d7dc5344f4463676bbd486d6521c70c24ad152b
Gleb Dolgich <glebd70@gmail.com>
2024-07-03 15:13:24 +0100

    Implement O(n) solution

--- tut_squares_of_sorted_array/main.cpp
+++ tut_squares_of_sorted_array/main.cpp
@@ -1,5 +1,5 @@
-#include <vector>
 #include <algorithm>
+#include <vector>
 
 #include <gtest/gtest.h>
 
@@ -14,14 +14,50 @@ public:
   {
     for (int& n: nums)
     {
-      n = n*n;
+      n = n * n;
     }
     std::sort(std::begin(nums), std::end(nums));
     return nums;
   }
 };
 
-TEST(SquaresOfSortedArray, Test1)
+// Runtime: 11 ms (beats 98%)
+// Memory Usage: 28.5 MB (beats 65%)
+class OptSolution
+{
+public:
+  static std::vector<int> sortedSquares(std::vector<int>& nums)
+  {
+    size_t const size = nums.size();
+    if (!size)
+      return nums;
+    std::vector<int> res(size);
+    size_t i1 = 0;
+    size_t i2 = size - 1;
+    int i = i2;
+    while (i >= 0)
+    {
+      int n1 = nums[i1];
+      int n2 = nums[i2];
+      int n1sq = n1 * n1;
+      int n2sq = n2 * n2;
+      if (n1sq >= n2sq)
+      {
+        res[i] = n1sq;
+        ++i1;
+      }
+      else
+      {
+        res[i] = n2sq;
+        --i2;
+      }
+      --i;
+    }
+    return res;
+  }
+};
+
+TEST(SquaresOfSortedArrayTrivial, Test1)
 {
   std::vector<int> nums = {-4, -1, 0, 3, 10};
   std::vector<int> expected = {0, 1, 9, 16, 100};
@@ -29,7 +65,7 @@ TEST(SquaresOfSortedArray, Test1)
   ASSERT_EQ(actual, expected);
 }
 
-TEST(SquaresOfSortedArray, Test2)
+TEST(SquaresOfSortedArrayTrivial, Test2)
 {
   std::vector<int> nums = {-7, -3, 2, 3, 11};
   std::vector<int> expected = {4, 9, 9, 49, 121};
@@ -37,10 +73,34 @@ TEST(SquaresOfSortedArray, Test2)
   ASSERT_EQ(actual, expected);
 }
 
-TEST(SquaresOfSortedArray, Test3)
+TEST(SquaresOfSortedArrayTrivial, Test3)
 {
   std::vector<int> nums = {1};
   std::vector<int> expected = {1};
   auto const actual = TrivialSolution::sortedSquares(nums);
+  ASSERT_EQ(actual, expected);
+}
+
+TEST(SquaresOfSortedArrayOpt, Test1)
+{
+  std::vector<int> nums = {-4, -1, 0, 3, 10};
+  std::vector<int> expected = {0, 1, 9, 16, 100};
+  auto const actual = OptSolution::sortedSquares(nums);
+  ASSERT_EQ(actual, expected);
+}
+
+TEST(SquaresOfSortedArrayOpt, Test2)
+{
+  std::vector<int> nums = {-7, -3, 2, 3, 11};
+  std::vector<int> expected = {4, 9, 9, 49, 121};
+  auto const actual = OptSolution::sortedSquares(nums);
+  ASSERT_EQ(actual, expected);
+}
+
+TEST(SquaresOfSortedArrayOpt, Test3)
+{
+  std::vector<int> nums = {1};
+  std::vector<int> expected = {1};
+  auto const actual = OptSolution::sortedSquares(nums);
   ASSERT_EQ(actual, expected);
 }
